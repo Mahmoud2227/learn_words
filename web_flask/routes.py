22da96cd59cf65c60fda7423132c3ddbea094ab4
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2023-04-04 20:24:02 +0200

    add the landing page

--- web_flask/routes.py
+++ web_flask/routes.py
@@ -2,7 +2,11 @@ from web_flask import app
 from flask import render_template
 from web_flask.models import List
 
-@app.route('/list')
-def market_page():
+@app.route('/')
+def landing_page():
+    return render_template('landing.html',css_file = 'landing')
+
+@app.route('/home')
+def home_page():
     lists = List.query.all()
-    return render_template('market.html', lists=lists)
+    return render_template('home.html', lists=lists)
